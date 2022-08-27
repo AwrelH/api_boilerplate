08e75c225d9da9dcf74718e6a1bab1a81b34f1fd
AwrelH <hiwa.awrel@gmail.com>
2022-08-27 21:19:30 +0000

    add post request finished using javascript fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,60 @@
-const API_KEY = "PeOIHLC_409ivMeu5OeRe2YjZPI";
+const API_KEY = "PeOIHLC_409ivMeu5OeRe2YjZPI"; // made key a variable
 
-const API_URL = "https://ci-jshint.herokuapp.com/api";
+const API_URL = "https://ci-jshint.herokuapp.com/api"; // made url a variable
 
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'))
 
-document.getElementById('status').addEventListener('click', e => getStatus(e));
+document.getElementById('status').addEventListener('click', e => getStatus(e)); //eventlisteners for buttons
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 
 
 
 
-async function postForm(e) {
+async function postForm(e) { /// async function for post and get
     const form = new FormData(document.getElementById('checksform'))
 
-    const response = await fetch(API_URL, {
+    const response = await fetch(API_URL, { // add await
         method: "POST",
         headers: {
                     "Authorization": API_KEY,
                  },
                  body: form,
         })
-}                        
+        const data = await response.json(); //response made to json format
+        if (response.ok){ //when data is reciveied 
+           displayErrors(data)
+        } else {
+            throw new Error(data.error)
+        }
+}       
+function displayErrors(data){ //display error
+    let heading = `JSHint Results for ${data.file}}`
+    if(data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>` //the count of errors
+        
+        for(let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `; //message line with col and line
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`; //the actual error
+        }
+    }
+        document.getElementById('resultsModalTitle').innerHTML = heading; //manipulating dom
+        document.getElementById('results-content').innerHTML = results;     //manipulating dom
 
-async function getStatus(e) {
+        resultsModal.show();
+}
+
+async function getStatus(e) {//get function to se if key is ok
     const queryString = `${API_URL}?api_key=${API_KEY}`
 
-    const response = await fetch(queryString);
+    const response = await fetch(queryString); //add await
 
-    const data = await response.json();
+    const data = await response.json(); //response to json and into const data
 
     if(response.ok) {
-       displayStatus(data);
+       displayStatus(data); // respons is displayed or else error info is messaged
     } else {
         throw new Error(data.error);
     }
